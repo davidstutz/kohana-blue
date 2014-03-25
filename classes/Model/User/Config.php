d0c9ddcd26d24d8eaf1f68034f1f7ee203d1f5cb
David Stutz <davidstutz@web.de>
2014-03-25 23:12:36 +0100

    Updated license information.

--- classes/Model/User/Config.php
+++ classes/Model/User/Config.php
@@ -4,7 +4,7 @@
  * 
  * @package		Blue
  * @author		David Stutz
- * @copyright	(c) 2013 David Stutz
+ * @copyright	(c) 2013 - 2014 David Stutz
  * @license		http://opensource.org/licenses/bsd-3-clause
  */
 class Model_User_Config extends Model_Blue_User_Config {}
